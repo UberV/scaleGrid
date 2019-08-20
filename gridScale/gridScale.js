c15bd5f756f4225ce97136174205a9ac79e2f30c
UberV <47228085+UberV@users.noreply.github.com>
2019-08-20 18:09:23 -0400

    Corrected bar positioning
    Fixed the position of the tool bar to better fit the new addition to foundrys tool bar. If someone knows how to anchor this toolbar to the bottom of foundrys let me know please.

--- gridScale/gridScale.js
+++ gridScale/gridScale.js
@@ -1,6 +1,6 @@
 /**
  * @author UberV
- * @version 0.0.5
+ * @version 0.0.6
  */
 
  class CustomButtons extends Application {     //This class defines the new set of buttons that we are using for this module. Pulled from foundry.js and modified to the best of my understanding.
